8fb864ef01766a9c04ca5fe5643f01566590a682
Mukul Kumar Sahu <MukulKumar.Sahu@triconinfotech.com>
2024-09-16 15:41:27 +0530

    fix

--- app/main.py
+++ app/main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI, HTTPException , UploadFile, File, Form
 from PyPDF2 import PdfReader
 from .langchain_model import extract_answer
 from .gemini_model import extract_answer_with_gemini
-from . import schemas
 
 app=FastAPI()
 
-# pdf_path = "gpt-4.pdf"
+pdf_path = "gpt-4.pdf"
 
 def extract_text_from_pdf(pdf_file) -> str:
     try:
@@ -20,28 +19,27 @@ def extract_text_from_pdf(pdf_file) -> str:
         raise HTTPException(status_code=500, detail=f"Error reading PDF: {str(e)}")
 
 
-# @app.get('/get')
-# def get_data_from_pdf():
-#     return (extract_text_from_pdf())
+@app.get('/get')
+def get_data_from_pdf():
+    return (extract_text_from_pdf(pdf_path))
 
 
 @app.post("/extract-answer/")
-async def extract_answer_from_pdf( file: UploadFile = File(...), question: str = Form(...)):
-    pdf_text = extract_text_from_pdf(file.file)
+async def extract_answer_from_pdf( my_file: UploadFile = File(...), question: str = Form(...)):
+    pdf_text = extract_text_from_pdf(my_file.file)
     answer = extract_answer(pdf_text, question)
     return {"question": question, "answer": answer}
 
 
 @app.post("/upload-pdf/")
-async def upload_pdf(file: UploadFile = File(...)):
-    # Extract text from the uploaded PDF
-    pdf_text = extract_text_from_pdf(file.file)
+async def upload_pdf(my_file: UploadFile = File(...)):
+    pdf_text = extract_text_from_pdf(my_file.file)
     return {"pdf_text": pdf_text}
 
 
 
 @app.post ("/extract_from_gemini")
-async def extract_answer_using_gemini( file: UploadFile = File(...) , question: str = Form(...) ):
-    pdf_text = extract_text_from_pdf(file.file)
+async def extract_answer_using_gemini( my_file: UploadFile = File(...) , question: str = Form(...) ):
+    pdf_text = extract_text_from_pdf(my_file.file)
     answer = extract_answer_with_gemini(pdf_text, question)
     return {"question": question, "answer": answer}
